0a85fc427d87ae913a888cecebdb5c055b05dfdb
Admin <h4x3r@hotmail.com>
2023-10-27 16:31:26 -0400

    UI fix and new JSON format

--- RoguelikeManager/Form1.Designer.cs
+++ RoguelikeManager/Form1.Designer.cs
@@ -28,101 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRefreshSaves = new System.Windows.Forms.Button();
-            this.lbSaves = new System.Windows.Forms.ListBox();
-            this.btnBackup = new System.Windows.Forms.Button();
-            this.btnRestore = new System.Windows.Forms.Button();
-            this.btnLaunch = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            btnRefreshSaves = new Button();
+            lbSaves = new ListBox();
+            btnBackup = new Button();
+            btnRestore = new Button();
+            btnLaunch = new Button();
+            btnDelete = new Button();
+            SuspendLayout();
             // 
             // btnRefreshSaves
             // 
-            this.btnRefreshSaves.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnRefreshSaves.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnRefreshSaves.Location = new System.Drawing.Point(27, 20);
-            this.btnRefreshSaves.Name = "btnRefreshSaves";
-            this.btnRefreshSaves.Size = new System.Drawing.Size(137, 59);
-            this.btnRefreshSaves.TabIndex = 0;
-            this.btnRefreshSaves.Text = "Reload";
-            this.btnRefreshSaves.UseVisualStyleBackColor = true;
-            this.btnRefreshSaves.Click += new System.EventHandler(this.btnRefreshSaves_Click);
+            btnRefreshSaves.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRefreshSaves.ForeColor = SystemColors.ActiveCaptionText;
+            btnRefreshSaves.Location = new Point(27, 20);
+            btnRefreshSaves.Name = "btnRefreshSaves";
+            btnRefreshSaves.Size = new Size(137, 59);
+            btnRefreshSaves.TabIndex = 0;
+            btnRefreshSaves.Text = "RELOAD";
+            btnRefreshSaves.UseVisualStyleBackColor = true;
+            btnRefreshSaves.Click += btnRefreshSaves_Click;
             // 
             // lbSaves
             // 
-            this.lbSaves.BackColor = System.Drawing.SystemColors.InfoText;
-            this.lbSaves.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbSaves.ForeColor = System.Drawing.SystemColors.Window;
-            this.lbSaves.FormattingEnabled = true;
-            this.lbSaves.ItemHeight = 37;
-            this.lbSaves.Location = new System.Drawing.Point(27, 108);
-            this.lbSaves.Name = "lbSaves";
-            this.lbSaves.Size = new System.Drawing.Size(473, 670);
-            this.lbSaves.TabIndex = 1;
+            lbSaves.BackColor = SystemColors.InfoText;
+            lbSaves.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            lbSaves.ForeColor = SystemColors.Window;
+            lbSaves.FormattingEnabled = true;
+            lbSaves.ItemHeight = 37;
+            lbSaves.Location = new Point(27, 108);
+            lbSaves.Name = "lbSaves";
+            lbSaves.Size = new Size(473, 670);
+            lbSaves.TabIndex = 1;
             // 
             // btnBackup
             // 
-            this.btnBackup.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnBackup.Location = new System.Drawing.Point(624, 108);
-            this.btnBackup.Name = "btnBackup";
-            this.btnBackup.Size = new System.Drawing.Size(128, 100);
-            this.btnBackup.TabIndex = 2;
-            this.btnBackup.Text = "BACKUP";
-            this.btnBackup.UseVisualStyleBackColor = true;
-            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            btnBackup.ForeColor = SystemColors.ActiveCaptionText;
+            btnBackup.Location = new Point(624, 108);
+            btnBackup.Name = "btnBackup";
+            btnBackup.Size = new Size(128, 100);
+            btnBackup.TabIndex = 2;
+            btnBackup.Text = "BACKUP";
+            btnBackup.UseVisualStyleBackColor = true;
+            btnBackup.Click += btnBackup_Click;
             // 
             // btnRestore
             // 
-            this.btnRestore.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnRestore.Location = new System.Drawing.Point(624, 400);
-            this.btnRestore.Name = "btnRestore";
-            this.btnRestore.Size = new System.Drawing.Size(128, 100);
-            this.btnRestore.TabIndex = 3;
-            this.btnRestore.Text = "RESTORE";
-            this.btnRestore.UseVisualStyleBackColor = true;
-            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            btnRestore.ForeColor = SystemColors.ActiveCaptionText;
+            btnRestore.Location = new Point(624, 400);
+            btnRestore.Name = "btnRestore";
+            btnRestore.Size = new Size(128, 100);
+            btnRestore.TabIndex = 3;
+            btnRestore.Text = "RESTORE";
+            btnRestore.UseVisualStyleBackColor = true;
+            btnRestore.Click += btnRestore_Click;
             // 
             // btnLaunch
             // 
-            this.btnLaunch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnLaunch.Location = new System.Drawing.Point(624, 692);
-            this.btnLaunch.Name = "btnLaunch";
-            this.btnLaunch.Size = new System.Drawing.Size(128, 100);
-            this.btnLaunch.TabIndex = 4;
-            this.btnLaunch.Text = "LAUNCH";
-            this.btnLaunch.UseVisualStyleBackColor = true;
-            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            btnLaunch.ForeColor = SystemColors.ActiveCaptionText;
+            btnLaunch.Location = new Point(624, 692);
+            btnLaunch.Name = "btnLaunch";
+            btnLaunch.Size = new Size(128, 100);
+            btnLaunch.TabIndex = 4;
+            btnLaunch.Text = "LAUNCH";
+            btnLaunch.UseVisualStyleBackColor = true;
+            btnLaunch.Click += btnLaunch_Click;
             // 
             // btnDelete
             // 
-            this.btnDelete.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnDelete.Location = new System.Drawing.Point(363, 20);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(137, 59);
-            this.btnDelete.TabIndex = 5;
-            this.btnDelete.Text = "DELETE";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            btnDelete.ForeColor = SystemColors.ActiveCaptionText;
+            btnDelete.Location = new Point(363, 20);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(137, 59);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "DELETE";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.MidnightBlue;
-            this.ClientSize = new System.Drawing.Size(837, 838);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnLaunch);
-            this.Controls.Add(this.btnRestore);
-            this.Controls.Add(this.btnBackup);
-            this.Controls.Add(this.lbSaves);
-            this.Controls.Add(this.btnRefreshSaves);
-            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Name = "Form1";
-            this.ShowIcon = false;
-            this.Text = "Frogcomposband Manager";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MidnightBlue;
+            ClientSize = new Size(837, 838);
+            Controls.Add(btnDelete);
+            Controls.Add(btnLaunch);
+            Controls.Add(btnRestore);
+            Controls.Add(btnBackup);
+            Controls.Add(lbSaves);
+            Controls.Add(btnRefreshSaves);
+            ForeColor = SystemColors.ControlLightLight;
+            Name = "Form1";
+            ShowIcon = false;
+            Text = "Frogcomposband Manager";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
